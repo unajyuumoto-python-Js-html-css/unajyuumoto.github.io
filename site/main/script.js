4fe599fd05bc21b1c857d0cd2a7896756c656c9e
unajyuumoto-python-Js-html-css <m.motoharu_815@outlook.jp>
2025-09-07 15:52:51 +0900

    一部削除 script.js

--- site/main/script.js
+++ site/main/script.js
@@ -44,54 +44,10 @@ el.scrollIntoView({ behavior: 'smooth', block: 'start' });
 
 
 
-function loadScratchComments(){
-  let comments = document.querySelector("#scratch-comments")
- 
-  //コメント取得
-  let response = await fetch("https://api.scratch.mit.edu/users/unajyuumoto/projects/1213761580/comments?limit=40&offset=0")
-      .then(res => res.json())
 
 
-  //for(let i of txt ){ ミスって作ったやつなんやで
-  //    console.log(i)
-  //}
 
-  let comment_id
-  let comment_arr = []
 
-  //コメントとりあえずコンソール出力（デバッグ時優秀
-  for(let i in response){
-      //console.log(response[i].content)
-      comment_arr.push(" >>>"+response[i].content)
-
-      comment_id = response[i].id
-
-      //console.log(comment_id)
-
-      let replyResponse = await fetch("https://api.scratch.mit.edu/users/unajyuumoto/projects/1213761580/comments/"+String(comment_id)+"/replies?limit=20&offset=0")
-      .then(res => res.json())
-      
-      //console.log("https://api.scratch.mit.edu/users/unajyuumoto/projects/1213761580/comments/"+String(comment_id)+"/replies?limit=20&offset=0")
-      //console.log(replyResponse)
-      
-      for(let j in replyResponse){
-          //console.log(" >>>"+replyResponse[j].content)
-          comment_arr.push(" >>>"+replyResponse[j].content)
-      }
-  }
-
-  //console.log(comment_arr)
-
-  for(i in comment_arr){
-      let comment_txt = document.createElement("div")
-      comment_txt.textContent = comment_arr[i]
-      
-      comment_txt.className = "comment"
-
-      comments.appendChild(comment_txt)
-    }}
-
-loadScratchComments()
 
 
 
